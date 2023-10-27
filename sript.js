a8337f6888f6e208ea467fbfc41fe699bfe48923
S.Rohini <149063247+rohini593@users.noreply.github.com>
2023-10-26 22:27:16 -0700

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,13 @@
+// sections
+
+
+// scroll sections
+let sections
+window.onscroll =() => {
+    // home section
+    let home =document.querySelector('home')
+
+    Headers.class.sections(home,window,srolly>100)
+}
+
+
